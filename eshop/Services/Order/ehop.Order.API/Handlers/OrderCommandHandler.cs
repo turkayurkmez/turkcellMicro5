c13521ca9864c9919057c05d961b2bf731d7ec72
Türkay Ürkmez <turkay@turkayurkmez.com>
2022-12-28 15:25:01 +0300

    adding demo with containers

---
+++ eshop/Services/Order/ehop.Order.API/Handlers/OrderCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using ehop.Order.API.Commands;
+
+namespace ehop.Order.API.Handlers
+{
+    public class OrderCommandHandler : ICommandHandler<CreateOrderCommand>,
+                                       ICommandHandler<UpdateOrderStatusToFailedCommand>,
+                                       ICommandHandler<OrderCanceledCommand>
+    {
+        public void Handle(CreateOrderCommand command)
+        {
+
+            //db'ye yazılıyor.
+        }
+
+        public void Handle(UpdateOrderStatusToFailedCommand command)
+        {
+            //db'de status failed olarak güncellenecek
+        }
+
+        public void Handle(OrderCanceledCommand command)
+        {
+            //müşterinin siparişini bul ve iptal et.
+        }
+    }
+}
